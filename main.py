1f3199a275e985f3d69cb35d38b5a6c7897f4704
amsv <ammosovaleksey@ya.ru>
2021-11-21 20:51:28 +0300

    эхо бот  2 варианта

---
+++ main.py
@@ -0,0 +1,4 @@
+import asyncio
+
+from aiogram import Bot, dispatcher, executor
+from config import BOT_TOKEN
--- test.py
+++ test.py
@@ -1,5 +1,16 @@
-import  openpyxl
+import requests
+import import_config
 
-print("Привет")
+api_link = "https://api.telegram.org/bot"+ import_config.token
 
-print("Привет!!!")
+
+updates = requests.get(api_link + "/getUpdates?offset=-1").json()
+
+print(updates)
+
+message = updates["result"][0]["message"]
+
+chat_id = message["from"]["id"]
+text =  message["text"]
+
+sent_message  = requests.get(api_link + f"/sendMessage?chat_id={chat_id}&text=Привет ты написал {text}") 
