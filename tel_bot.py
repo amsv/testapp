39617e4b50858bb8724e5debd3b0239c68dc58ca
amsv <ammosovaleksey@ya.ru>
2021-11-23 22:39:28 +0300

    +клиент админ. Прокал конекст

--- tel_bot.py
+++ tel_bot.py
@@ -1,15 +1,35 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
+from aiogram.types import message
 from aiogram.utils import executor
 
 from config import BOT_TOKEN
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
+#**********************клиент********************
+@dp.message_handler(commands=['start', 'help'])
+async def command_start(message : types.Message):    
+    try:
+        await bot.send_message(message.from_user.id, 'поехали!')
+        await message.delete()
+    except:
+        await message.reply('общение с ботом через ЛС')
 
+@dp.message_handler(commands=['Режим_работы'])
+async def command_start(message : types.Message):    
+    await bot.send_message(message.from_user.id, 'С 8:30  до 17:30')
+
+@dp.message_handler(commands=['Расположение'])
+async def command_start(message : types.Message):    
+    await bot.send_message(message.from_user.id, 'Сыктывкар')
+
+#*********************админ*********************
+#***********************************************
 @dp.message_handler()
 async def echo_send(message : types.Message):
-    await message.answer(message.text)
+    if message.text == 'Привет':
+        await message.answer(f"И тебе привет")
     #await message.reply(message.text)
     #await bot.send_message(message.from_user.id, message.text)
 
