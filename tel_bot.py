a78142fc6412318daeafff0863dd42892f10b9bc
amsv <ammosovaleksey@ya.ru>
2021-11-23 23:31:24 +0300

    +

--- tel_bot.py
+++ tel_bot.py
@@ -7,6 +7,11 @@ from config import BOT_TOKEN
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
+
+################# на  старт в  консоль
+#async def on_startup( ):
+#    print('Бот  в  онлайн')
+
 #**********************клиент********************
 @dp.message_handler(commands=['start', 'help'])
 async def command_start(message : types.Message):    
@@ -33,4 +38,5 @@ async def echo_send(message : types.Message):
     #await message.reply(message.text)
     #await bot.send_message(message.from_user.id, message.text)
 
+#executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
 executor.start_polling(dp, skip_updates=True)
